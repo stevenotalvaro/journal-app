d2c72d4bf4b89216783907ef8a1f31b342982d65
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-08-18 14:18:53 -0500

    inital proyect

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {JournalApp} from './JournalApp'
+import './styles/styles.scss'
+ReactDOM.render(
+  <React.StrictMode>
+    <JournalApp />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
