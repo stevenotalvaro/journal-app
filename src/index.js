89573e1b3454b88048cc5f4b692188a1db635f4c
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-08-18 14:44:11 -0500

    Router implemented

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import {JournalApp} from './JournalApp'
 import './styles/styles.scss'
 ReactDOM.render(
-  <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <JournalApp />
+    </React.StrictMode>,
+    document.getElementById('root'),
 )
