27617112bc0c0b3a6417c5c67cea0a1464917cc1
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-08-19 20:02:48 -0500

    Start Login And Email implemented

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,9 +2,30 @@ import {types} from '../components/types/types'
 import {firebase, googleAuthProvider} from '../firebase/firebase-config'
 export const startLoginEmailPassword = (email, password) => {
     return dispatch => {
-        setTimeout(() => {
-            dispatch(login(123, 'antonio'))
-        }, 3000)
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(({user}) => {
+                dispatch(login(user.uid, user.displayName))
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+}
+
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    return dispatch => {
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(async ({user}) => {
+                await user.updateProfile({displayName: name})
+                dispatch(login(user.uid, user.displayName))
+            })
+            .catch(e => {
+                console.log(e)
+            })
     }
 }
 
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {useForm} from '../../hooks/useForm'
 import validator from 'validator'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {removeError, setError} from '../../actions/ui'
+import {startRegisterWithEmailPasswordName} from '../../actions/auth'
 
 export const RegisterScreen = () => {
     const dispatch = useDispatch()
+    const {msgError} = useSelector(state => state.ui)
+    console.log(msgError)
     const [formValues, handleInputChange] = useForm({
         name: 'Steven',
         email: 'steven@gmail.com',
@@ -19,7 +22,7 @@ export const RegisterScreen = () => {
     const handleRegister = e => {
         e.preventDefault()
         if (isFormValid()) {
-            dispatch(setError('hola mundo'))
+            dispatch(startRegisterWithEmailPasswordName(email, password, name))
         }
     }
 
@@ -43,7 +46,9 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={handleRegister}>
-                <div className="auth__alert-error">Hola Mundo</div>
+                {msgError && (
+                    <div className="auth__alert-error">{msgError}</div>
+                )}
                 <input
                     type="text"
                     placeholder="Name"
