92cfe3864346b97e5384bf443e1af81e1505c283
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-08-25 14:31:20 -0500

    select the note from sidebar and show it in the NoteScreen

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,31 +1,42 @@
 import React from 'react'
+import moment from 'moment'
+import {useDispatch} from 'react-redux'
+import {activeNote} from '../../actions/notes'
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, body, title, url}) => {
+    const dispatch = useDispatch()
+    const noteDate = moment(date)
+    const handleEntryclick = () => {
+        dispatch(
+            activeNote(id, {
+                date,
+                body,
+                title,
+                url,
+            }),
+        )
+    }
     return (
-        <div className="journal__entry pointer">
-            
-            <div 
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://earthsky.org/upl/2018/12/comet-wirtanen-Jack-Fusco-dec-2018-Anza-Borrego-desert-CA-e1544613895713.jpg)'
-                }}
-            ></div>
+        <div onClick={handleEntryclick} className="journal__entry pointer">
+            {url && (
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: 'url(`url`)',
+                    }}
+                ></div>
+            )}
 
             <div className="journal__entry-body">
-                <p className="journal__entry-title">
-                    Un nuevo día
-                </p>
-                <p className="journal__entry-content">
-                    Reprehenderit id in duis consectetur deserunt veniam fugiat.
-                </p>
+                <p className="journal__entry-title">{title}</p>
+                <p className="journal__entry-content">{body}</p>
             </div>
 
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format('dddd')}</span>
+                <h4>{noteDate.format('Do')}</h4>
             </div>
-
         </div>
     )
 }
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,36 +1,44 @@
 import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import {useSelector} from 'react-redux'
+import {useForm} from '../../hooks/useForm'
+import {NotesAppBar} from './NotesAppBar'
 
 export const NoteScreen = () => {
+    const {active: note} = useSelector(state => state.notes)
+    const [formValues, handleInputChange] = useForm(note)
+    const {body, title, url} = formValues
     return (
         <div className="notes__main-content">
-            
             <NotesAppBar />
 
             <div className="notes__content">
-
-                <input 
+                <input
                     type="text"
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    value={title}
+                    name="title"
+                    onChange={handleInputChange}
                 />
 
                 <textarea
                     placeholder="What happened today"
                     className="notes__textarea"
+                    value={body}
+                    name="body"
+                    onChange={handleInputChange}
                 ></textarea>
 
-                <div className="notes__image">
-                    <img 
-                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-                        alt="imagen"
-                    />
-                </div>
-
-
+                {url && (
+                    <div className="notes__image">
+                        <img
+                            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                            alt="imagen"
+                        />
+                    </div>
+                )}
             </div>
-
         </div>
     )
 }
